4dd5e1dedead2fd2454f89b3d206cabe8009eef0
nandafitra <fitrananda05@gmail.com>
2023-01-13 16:04:12 +0700

    menambahkan react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,22 @@ import ProductList from "./pages/ProductList/ProductList";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Cart from "./pages/Cart/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Cart />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Register />} path="/register" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Cart />} path="/cart" />
+        <Route element={<ProductPage />} path="/product-page/:ProductId" />
+        <Route element={<ProductList />} path="/product-list" />
+        {/* <Route component={Admin} path="/admin" /> */}
+        <Route element={<Home />} path="/" />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
